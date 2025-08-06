a84eeef219f49647b6351878b2c2d955f0202c93
sedik <sedik91270@hotmail.fr>
2025-08-06 18:20:29 +0200

    Ajout de l’aperçu des images sélectionnées

--- frontend/app.js
+++ frontend/app.js
@@ -2,6 +2,18 @@ const input = document.querySelector("input");
 const previsualise = document.getElementById("previewContainer");
 const genererBtn = document.getElementById("generatePdfBtn");
 
-input.addEventListener("change", (e) => {
+input.addEventListener("change", () => {
+  previsualise.innerHTML = "";
+  const fichiers = input.files;
+
+  Array.from(fichiers).forEach((fichier) => {
+    const imageURL = URL.createObjectURL(fichier);
+
+    const img = document.createElement("img");
+
+    img.src = imageURL;
+    previsualise.appendChild(img);
+  });
+
   console.log(input.files);
 });
