57c7567aafc10d9af1e9cdcc800abb6c28313ae7
sedik <sedik91270@hotmail.fr>
2025-08-07 16:27:07 +0200

    Correction du bug PNG et ajout de l’export PDF fonctionnel

--- frontend/app.js
+++ frontend/app.js
@@ -29,4 +29,42 @@ genererBtn.addEventListener("click", () => {
     const clone = image.cloneNode(true);
     imageRight.appendChild(clone);
   });
+
+  const premiereImage = imageRight.querySelector("img");
+
+  // Crée un nouvel objet Image à partir du src
+  const tempImage = new Image();
+  tempImage.src = premiereImage.src;
+
+  tempImage.onload = () => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = tempImage.naturalWidth;
+    canvas.height = tempImage.naturalHeight;
+
+    ctx.drawImage(tempImage, 0, 0);
+
+    const imageData = canvas.toDataURL("image/png");
+
+    const doc = new jspdf.jsPDF({
+      orientation: "portrait",
+      unit: "mm",
+      format: "a4",
+    });
+
+    const pageWidth = doc.internal.pageSize.getWidth();
+    const pageHeight = doc.internal.pageSize.getHeight();
+
+    const ratio = Math.min(
+      pageWidth / canvas.width,
+      pageHeight / canvas.height
+    );
+
+    const imgWidth = canvas.width * ratio;
+    const imgHeight = canvas.height * ratio;
+
+    doc.addImage(imageData, "PNG", 10, 10, imgWidth, imgHeight);
+    doc.save("images.pdf");
+  };
 });
