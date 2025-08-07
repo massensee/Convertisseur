5f54ca25e72ee068de5fee69664e588df5b64e62
sedik <sedik91270@hotmail.fr>
2025-08-07 16:02:46 +0200

    gérer l'affichage des images

--- frontend/app.js
+++ frontend/app.js
@@ -1,9 +1,11 @@
 const input = document.querySelector("input");
 const previsualise = document.getElementById("previewContainer");
+const imageLeft = document.getElementById("previewLeft");
+const imageRight = document.getElementById("previewRight");
 const genererBtn = document.getElementById("generatePdfBtn");
 
 input.addEventListener("change", () => {
-  previsualise.innerHTML = "";
+  imageLeft.innerHTML = "";
   const fichiers = input.files;
 
   Array.from(fichiers).forEach((fichier) => {
@@ -12,8 +14,19 @@ input.addEventListener("change", () => {
     const img = document.createElement("img");
 
     img.src = imageURL;
-    previsualise.appendChild(img);
+    imageLeft.appendChild(img);
   });
 
   console.log(input.files);
+});
+
+genererBtn.addEventListener("click", () => {
+  imageRight.innerHTML = "";
+
+  const imagesCopiees = imageLeft.querySelectorAll("img");
+
+  imagesCopiees.forEach((image) => {
+    const clone = image.cloneNode(true);
+    imageRight.appendChild(clone);
+  });
 });
